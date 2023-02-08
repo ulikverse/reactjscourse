bf5e47dbb34480ef9c3736ecac8f62189269ca2d
Yulii Shtel <ulik1shtel@gmail.com>
2023-02-08 21:59:54 +0200

    ReactJS Course [2] - JSX, Components, Props...

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"> Hello World! </div>;
+  return (
+    <div className="App">
+      <User name="Ulik" age={27} email="ulik@ulik.com" />
+      <User name="Jake" age={29} email="jake@ulik.com" />
+      <User name="Jessica" age={190} email="jessica@ulik.com" />
+    </div>
+  );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <div>
+        <h1>{props.name}</h1>
+        <h1>{props.age}</h1>
+        <h1>{props.email}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default App;
