3300c527fc99418d14ce844df29059174b201380
Yulii Shtel <ulik1shtel@gmail.com>
2023-02-18 17:35:58 +0200

    ReactJS Course [5] - CRUD In React | TodoList Tutorial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import "./App.css";
 import { useState } from "react";
+import { Task } from "./Task";
 
 function App() {
-  const [textColor, setTextColor] = useState("black");
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="App">
-      <button
-        onClick={() => {
-          setTextColor(textColor === "black" ? "red" : "black");
-        }}
-      >
-        Show/Hide
-      </button>
-      <h1 style={{ color: textColor }}>HI MY NAME IS ULIK</h1>
+      <div className="addTask">
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <Task
+              taskName={task.taskName}
+              id={task.id}
+              deleteTask={deleteTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
