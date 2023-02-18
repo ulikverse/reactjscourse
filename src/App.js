9c75a124d345144d00f113fc8d11c72c85a8cbba
Yulii Shtel <ulik1shtel@gmail.com>
2023-02-18 18:19:04 +0200

    ReactJS Course [6] - Component Lifecycle | UseEffect Tutorial

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
 import "./App.css";
-import { useState } from "react";
-import { Task } from "./Task";
+import { useState, useEffect } from "react";
+import { Text } from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-    };
-    setTodoList([...todoList, task]);
-  };
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
+  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button onClick={addTask}>Add Task </button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              id={task.id}
-              deleteTask={deleteTask}
-            />
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        Show Text
+      </button>
+
+      {showText && <Text />}
     </div>
   );
 }
--- src/Task.js
+++
@@ -1,8 +0,0 @@
-export const Task = (props) => {
-  return (
-    <div className="task">
-      <h1>{props.taskName}</h1>
-      <button onClick={() => props.deleteTask(props.id)}> X </button>
-    </div>
-  );
-};
