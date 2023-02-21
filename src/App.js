59f25da74ff523d428a0ee6b3f7cb7ccabc59bb8
Yulii Shtel <ulik1shtel@gmail.com>
2023-02-21 20:36:32 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
-import { Contact } from "./pages/Contact";
-import { Error } from "./pages/Error";
-import { Navbar } from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
-    </div>
+    < className="App"></>
+      
   );
-}
+
 
 export default App;
--- src/pages/Home.js
+++
@@ -1,3 +0,0 @@
-export const Home = () => {
-  return <h1>THIS IS THE HOMEPAGE </h1>;
-};
