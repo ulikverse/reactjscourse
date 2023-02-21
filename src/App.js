1c37d550417db4a47c6960638643b3d90bc1de01
Yulii Shtel <ulik1shtel@gmail.com>
2023-02-21 20:49:38 +0200

    returned to a previous commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Error } from "./pages/Error";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
-    < className="App"></>
-      
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
   );
-
+}
 
 export default App;
